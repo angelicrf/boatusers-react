39e4f90110a2715444323e443eb912572c64e496
angelicrf <angelicrf@gmail.com>
2023-01-25 17:13:09 -0500

    initial-set-up

---
+++ boatusers/src/About.js
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <div className="App">
+            <header>
+                <title>About</title>
+            </header>
+            <div>Contact Me</div>
+        </div>
+    );
+}
---
+++ boatusers/src/App.js
@@ -0,0 +1,11 @@
+
+export default function App() {
+  return (
+    <div className="App">
+      <header>
+        <title>BoatUser</title>
+      </header>
+      <div>Welcome BoatUsers</div>
+    </div>
+  );
+}
