1e095586ac77d5729a38fe8fc6d200773e8f62e3
angelicrf <angelicrf@gmail.com>
2023-02-02 10:55:20 -0500

    client-dir-rename

--- build-compose.sh
+++ build-compose.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo docker-compose -f /home/angelique/Desktop/boatusers-react/boatusers/docker-compose.yml config > _tmp_A.yml
+sudo docker-compose -f $(pwd)/client-buApp/docker-compose.yml config > _tmp_A.yml
 
-sudo docker-compose -f /home/angelique/Desktop/boatusers-react/server-buApp/docker-compose.yml config > _tmp_B.yml
+sudo docker-compose -f $(pwd)/server-buApp/docker-compose.yml config > _tmp_B.yml
 
 sudo docker-compose \
   -f _tmp_A.yml \
