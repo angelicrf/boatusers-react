1070ccfdc659fc72c24743d09baca3815fc4457d
angelicrf <angelicrf@gmail.com>
2023-02-02 13:36:15 -0500

    find-userlocation-map

--- client-buApp/src/HooksComponents/BUMap.js
+++ client-buApp/src/HooksComponents/BUMap.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { displayMap, testFunc } from "../JS/mapData"
+import { displayMap, findMyLocation } from "../JS/mapData"
 
 export default function BUMap() {
     const [thisData, setThisData] = useState('')
@@ -33,7 +33,7 @@ export default function BUMap() {
             {(thisData !== null) ? <div>{thisData}</div> : (<div>Not Data</div>)}
 
             <div id="buMap" style={{ width: 400, height: 300 }}></div>
-            <button type="button" className="btn btn-primary" onClick={() => console.log(displayMap())}>Display Map</button>
+            <button type="button" className="btn btn-primary" onClick={() => findMyLocation()}>Display Map</button>
 
         </div>
     )
--- client-buApp/src/JS/mapData.js
+++ client-buApp/src/JS/mapData.js
@@ -2,11 +2,11 @@
 
 
 const mapboxgl = require('mapbox-gl')
+mapboxgl.accessToken =
+    //'pk.eyJ1IjoiYW5nZWxyZWYiLCJhIjoiY2w0czNxMTA2MGkzcjNqbzB5cjlkM3BkaSJ9.gpg4wdvg4dobgzcw795VQw'
+    'pk.eyJ1IjoiYW5nZWxyZWYiLCJhIjoiY2w0czNxMTA2MGkzcjNqbzB5cjlkM3BkaSJ9.gpg4wdvg4dobgzcw795VQw';
 
 const displayMap = () => {
-    mapboxgl.accessToken =
-        //'pk.eyJ1IjoiYW5nZWxyZWYiLCJhIjoiY2w0czNxMTA2MGkzcjNqbzB5cjlkM3BkaSJ9.gpg4wdvg4dobgzcw795VQw'
-        'pk.eyJ1IjoiYW5nZWxyZWYiLCJhIjoiY2w0czNxMTA2MGkzcjNqbzB5cjlkM3BkaSJ9.gpg4wdvg4dobgzcw795VQw';
     return new mapboxgl.Map({
         container: 'buMap',
         style: 'mapbox://styles/mapbox/streets-v12',
@@ -14,6 +14,20 @@ const displayMap = () => {
         zoom: 9
     });
 }
-
-const testFunc = () => console.log("myName")
-module.exports = { testFunc, displayMap }
+const findMyLocation = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+        return new mapboxgl.Map({
+            container: 'buMap',
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [position.coords.longitude, position.coords.latitude],
+            zoom: 13
+        }).addControl(new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true,
+            showUserHeading: true
+        }));
+    })
+}
+module.exports = { findMyLocation, displayMap }
