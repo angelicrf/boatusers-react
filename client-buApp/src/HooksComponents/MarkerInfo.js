9762706fbc97e84f2bbbc9b22b19ce374dc6e289
angelicrf <angelicrf@gmail.com>
2023-02-07 16:11:26 -0500

    fav-icon-addMarker

--- client-buApp/src/HooksComponents/MarkerInfo.js
+++ client-buApp/src/HooksComponents/MarkerInfo.js
@@ -1,13 +1,37 @@
 import { useLocation } from 'react-router-dom';
 import Carousel from 'react-bootstrap/Carousel';
 import ChildCarousel from '../FuncComponents/ChildCarousel';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 export default function MarkerInfo() {
+
+    const [favClicked, setFavClicked] = useState(false)
 
     const location = useLocation();
     const ref = useRef();
     ref.current = 0
+    let favObj = Object.assign({
+        myFav: {
+            favId: '',
+            favName: ''
+        }
+    })
+    let allFavorites = []
+    const addFavorite = (thisName, thisId) => {
+        setFavClicked(true)
+        favObj.myFav.favName = thisName
+        favObj.myFav.favId = thisId
+
+        allFavorites.push(favObj)
+        let sorted = allFavorites.filter((c, index) => {
+            return allFavorites.indexOf(c) === index
+        })
+        // local storage
+        console.log('clicked ', sorted)
+        return sorted
+    }
+    //useEffect to get from localstorage
+
     return (
 
         <div>
@@ -30,9 +54,10 @@ export default function MarkerInfo() {
                             /*<div key={index}><ChildCarousel buElement={element} buIndex={index} ref={ref} /></div> */
                         ))}
                     </Carousel>
-                    <h4 className='card-title'>{location.state.locName}</h4>
+                    <h4 className='card-title mt-1'>{location.state.locName}  <span><button style={{ float: 'right', backgroundColor: favClicked ? 'red' : 'blue' }} onClick={() => { addFavorite(location.state.locName, location.state.locId) }}><i className="bi bi-star"></i></button></span></h4>
                     {location.state.locCenter.map((thisCenter, index) => <div key={index} className="card-text">{thisCenter}</div>)}
                     <p className='card-text'>{location.state.locId}</p>
+
                 </div>
             </div>
         </div>
