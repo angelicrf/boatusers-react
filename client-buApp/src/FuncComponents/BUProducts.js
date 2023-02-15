7f255acd4e0669980eddaec392ae9a0bf55cbd38
Your Name <you@example.com>
2023-02-15 09:19:48 -0500

    products-inital

---
+++ client-buApp/src/FuncComponents/BUProducts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const BUProducts = () => {
+  return (
+    <div>
+      <header>
+        <title>Products</title>
+      </header>
+      <BUNavBar />
+      <div>BUProducts</div>
+      <UserName />
+    </div>
+  );
+};
--- client-buApp/src/index.js
+++ client-buApp/src/index.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './FuncComponents/App'
-import About from './FuncComponents/About'
-import UserInfo from './FuncComponents/UserInfo'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import reportWebVitals from './reportWebVitals'
-import BUWeather from './HooksComponents/BUWeather'
-import BUItems from './HooksComponents/BUItems'
-import BUAutomation from './HooksComponents/BUAutomation'
-import BUMap from './HooksComponents/BUMap'
-import { persistor, store } from '../src/Store/ReduxStore'
-import { PersistGate } from 'redux-persist/integration/react'
-import { Provider } from 'react-redux'
-import ContextProvider from './Store/ContextProvider'
-import MarkerInfo from './HooksComponents/MarkerInfo'
-import FavoritedPlaces from './FuncComponents/FavoritedPlaces'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './FuncComponents/App';
+import About from './FuncComponents/About';
+import UserInfo from './FuncComponents/UserInfo';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+import BUWeather from './HooksComponents/BUWeather';
+import BUItems from './HooksComponents/BUItems';
+import BUAutomation from './HooksComponents/BUAutomation';
+import BUMap from './HooksComponents/BUMap';
+import { persistor, store } from '../src/Store/ReduxStore';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import ContextProvider from './Store/ContextProvider';
+import MarkerInfo from './HooksComponents/MarkerInfo';
+import FavoritedPlaces from './FuncComponents/FavoritedPlaces';
+import { BUProducts } from './FuncComponents/BUProducts';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <>
@@ -25,17 +26,18 @@ root.render(
         <ContextProvider>
           <BrowserRouter>
             <Routes>
-              <Route path='/' element={<App />} />
-              <Route path='/Map' element={<BUMap />} />
-              <Route path='/About' element={<About />} />
-              <Route path='/Home' element={<App />} />
-              <Route path='/Items' element={<BUItems />} />
-              <Route path='/Weather' element={<BUWeather />} />
-              <Route path='/Automation' element={<BUAutomation />} />
-              <Route path='/MapLocInfo' element={<MarkerInfo />} />
-              <Route path='/MyAccount' element={<UserInfo />} />
+              <Route path="/" element={<App />} />
+              <Route path="/Map" element={<BUMap />} />
+              <Route path="/About" element={<About />} />
+              <Route path="/Home" element={<App />} />
+              <Route path="/Items" element={<BUItems />} />
+              <Route path="/Weather" element={<BUWeather />} />
+              <Route path="/Automation" element={<BUAutomation />} />
+              <Route path="/MapLocInfo" element={<MarkerInfo />} />
+              <Route path="/MyAccount" element={<UserInfo />} />
+              <Route path="/Products" element={<BUProducts />} />
               <Route
-                path='/MyAccount/FavoritePlaces'
+                path="/MyAccount/FavoritePlaces"
                 element={<FavoritedPlaces />}
               />
             </Routes>
@@ -43,7 +45,7 @@ root.render(
         </ContextProvider>
       </PersistGate>
     </Provider>
-  </>,
-)
+  </>
+);
 
-reportWebVitals()
+reportWebVitals();
