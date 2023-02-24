7fae4814da8362e6af9486ad73bd7fe295f511d4
angelicrf <angelicrf@gmail.com>
2023-02-24 12:27:51 -0500

    delivery-status

---
+++ client-buApp/src/FuncComponents/DeliveryStatus.js
@@ -0,0 +1,132 @@
+import React, { useEffect, useState } from 'react'
+
+const DeliveryStatus = () => {
+  const [progessbarValue, setProgressbarValue] = useState(0)
+  const [progressMsg, setProgresMsg] = useState('')
+  const [deliveryMsg, setDeliveryMsg] = useState('')
+  const [progessBtn, setProgressBtn] = useState(false)
+  const [cityName, setCityName] = useState('')
+  const [audio, setAudio] = useState(new Audio(''))
+  //https://actions.google.com/sounds/v1/cartoon/clang_and_wobble.ogg
+  useEffect(() => {
+    if (cityName !== undefined && progessBtn) {
+      updateProgressbarValue(mockData, cityName)
+      setProgressBtn(false)
+    }
+    if (audio.src !== 'http://localhost:3000/Cart') {
+      console.log(audio.src)
+      audio.play()
+      setAudio(new Audio(''))
+    }
+  }, [cityName, progessBtn])
+
+  // mongodb database based on the facilities lat and long and user destination
+  const mockData = [
+    {
+      city: ['Fort Lauderdale', 'Boca Raton', 'West Palm Beach', 'Miami'],
+    },
+  ]
+  const submitProgress = (e) => {
+    e.preventDefault()
+    console.log(cityName)
+    setProgressBtn(true)
+  }
+  const updateProgressbarValue = (thisMockData, thisCityName) => {
+    let thisValue = 1
+    if (thisMockData) {
+      thisMockData[0].city.map((md, i) => {
+        if (md.toLowerCase() == thisCityName.toLowerCase()) {
+          switch (thisCityName) {
+            case 'miami':
+              setAudio(
+                new Audio(
+                  'https://actions.google.com/sounds/v1/cartoon/clang_and_wobble.ogg',
+                ),
+              )
+              setProgressbarValue(thisValue * 10 * 10)
+              setDeliveryMsg('Package left the Facility')
+              setProgresMsg('Ordered')
+              break
+            case 'boca raton':
+              setAudio(
+                new Audio(
+                  'https://actions.google.com/sounds/v1/cartoon/cartoon_boing.ogg',
+                ),
+              )
+              setProgressbarValue(thisValue * 15 * 10)
+              setDeliveryMsg('Package arrived at a carrier facility')
+              setProgresMsg('Shipped')
+              break
+            case 'fort lauderdale':
+              setAudio(
+                new Audio(
+                  'https://actions.google.com/sounds/v1/cartoon/clown_slide_sound_effects.ogg',
+                ),
+              )
+              setProgressbarValue(thisValue * 20 * 10)
+              setDeliveryMsg(
+                'Package transfered to another facility to delivery',
+              )
+              setProgresMsg('Out for delivery')
+              break
+            case 'west palm beach':
+              setAudio(
+                new Audio(
+                  'https://actions.google.com/sounds/v1/cartoon/slap_with_glove.ogg',
+                ),
+              )
+              setProgressbarValue(thisValue * 25 * 10)
+              setDeliveryMsg('Package delivered')
+              setProgresMsg('Delivered')
+              break
+            default:
+              break
+          }
+        }
+      })
+    }
+  }
+
+  return (
+    <div>
+      <div className='container bg-info rounded mt-2 mb-2'>
+        <p>Enter one of these locations: 'Fort Lauderdale',</p>
+        <p> 'Boca Raton', 'West Palm Beach', 'Miami'</p>
+        <form onSubmit={submitProgress} name='signin_form'>
+          <input
+            type='text'
+            value={cityName}
+            required
+            placeholder='city name'
+            onChange={(e) => setCityName(e.target.value)}
+          />
+          <button className='btn btn-primary' type='submit'>
+            Search
+          </button>
+        </form>
+        <div className='py-2'>
+          Delivery Status:
+          <div className='progress'>
+            <div
+              className='progress-bar progress-bar-striped progress-bar-animated'
+              role='progressbar'
+              aria-valuenow={progessbarValue}
+              aria-valuemin='0'
+              aria-valuemax='100'
+              style={{ width: `${progessbarValue}px` }}
+            >
+              {progressMsg}
+            </div>
+          </div>
+        </div>
+        {deliveryMsg !== '' ? (
+          <div>
+            <div className='text-danger fw-bold'>{deliveryMsg}</div>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  )
+}
+
+export default DeliveryStatus
